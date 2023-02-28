eb8712c2eba47c3c738def532bff4d1bc6a48694
Joshua <janorris65@gmail.com>
2023-02-28 10:03:36 -0600

    dev reaction schema

--- models/reactions.js
+++ models/reactions.js
@@ -1,7 +1,35 @@
 import { Schema, model } from "mongoose";
 
+const reactionSchema = new Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      ref: "users",
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+    },
+    reactionBody: {
+      type: String,
+      required: true,
+      maxLength: 280,
+    },
+  },
+  {
+    strict: "throw",
+    // Send the virtuals to the client (e.g. res.json)
+    toJSON: { virtuals: true },
+    toObject: { virtuals: true },
+    versionKey: false,
+  }
+);
+
+export { reactionSchema };
+export default model("thoughts", reactionSchema);
 // reactions cont
-// reactionID mongooses Objectid datatype, default set to new ObjectID
+// reactionID mongooses Objectid datatype, default set to new ObjectID??? isnt this included
 // reactionBody string, required, 280 max
 // Username string, required
 // createdAt , Date, default to current time stamp, getter method to format timestamp
--- models/thoughts.js
+++ models/thoughts.js
@@ -1,5 +1,5 @@
 import { Schema, model } from "mongoose";
-// import reactionSchema from "./reactions.js"
+import reactionSchema from "./reactions.js";
 
 const thoughtSchema = new Schema(
   {
@@ -18,7 +18,7 @@ const thoughtSchema = new Schema(
       required: true,
       ref: "users",
     },
-    // reactions: [reactionSchema],
+    reactions: [reactionSchema],
   },
   {
     strict: "throw",
@@ -28,14 +28,10 @@ const thoughtSchema = new Schema(
     versionKey: false,
   }
 );
+
 export { thoughtSchema };
 export default model("thoughts", thoughtSchema);
 // thoughttext , string required, 1-280 char
 // createdAt , Date, default to current time stamp, getter method to format timestamp
 // username string, required
 // reactions, array of nested (embedded documents)
-// reactions cont
-// reactionID mongooses Objectid datatype, default set to new ObjectID
-// reactionBody string, required, 280 max
-// Username string, required
-// createdAt , Date, default to current time stamp, getter method to format timestamp
